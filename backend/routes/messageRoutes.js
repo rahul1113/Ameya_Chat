01986defec1faec78524e10b946d5e5b1e3c0b04
rahul burnwal <rahulb631@gmail.com>
2022-04-05 21:34:20 +0530

    auth space

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -3,7 +3,7 @@ const {
   allMessages,
   sendMessage,
 } = require("../controllers/messageControllers");
-const protect = require("../middleware/authMiddleware");
+const protect = require("../middleware/authmiddleware");
 
 const router = express.Router();
 
